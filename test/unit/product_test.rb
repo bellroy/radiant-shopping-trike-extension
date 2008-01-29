c786c98e3938124d459a42fe430810c57a821ca1
tobin <tobin@dec4ba91-d232-0410-a499-b0f1e7490611>
2008-01-29 07:28:02 +0000

    Add unit tests for the Product model. Tests validations introduced in r63.
    git-svn-id: https://svn.trike.com.au/code/radiant/extensions/shopping_trike@66 dec4ba91-d232-0410-a499-b0f1e7490611

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -11,4 +11,49 @@ class ProductTest < Test::Unit::TestCase
   def test_that_products_are_defined
     assert defined?(Product)
   end
+  
+  def test_that_a_product_with_no_code_is_invalid
+    product = product_with_code( nil )
+    assert ! product.save
+  end
+  
+  def test_that_a_product_with_no_price_is_invalid
+    product = product_with_price( nil )
+    assert ! product.save
+  end
+  
+  def test_that_a_product_with_a_negative_price_is_invalid
+    product = product_with_price( -0.99 )
+    assert ! product.save
+  end
+  
+  def test_that_a_product_with_a_zero_price_is_invalid
+    product = product_with_price( 0.00 )
+    assert ! product.save
+  end
+  
+  def test_that_a_product_with_a_resrved_name_is_invalid
+    %w{checkout eula cart}.each do |reserved_code|
+      # create a product with a resrved code and assert that it doesn't save
+      product = product_with_code( reserved_code )
+      assert ! product.save
+    end
+  end
+  
+  private 
+    def product_with_code( code )
+      product = Product.new
+      product.code = code
+      product.price = 0.99
+      product.description = "A test product!"
+      product
+    end
+    
+    def product_with_price( price )
+      product = Product.new
+      product.code = "test-item"
+      product.description = "A test item with a given price!"
+      product.price = price
+      product
+    end
 end
